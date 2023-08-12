0a86f34f359539ed1185207dab9f8dc8d9514f91
vlixz <vlixz-programming@pm.me>
2023-08-12 13:16:24 +0200

    Project template v0.1

---
+++ examples/example1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "functions.h"
+
+int main() {
+
+    printf("Hello world!\n");
+    
+    printf("%d\n\n", add(1, 6));
+
+    return 1;
+}
---
+++ include/functions.h
@@ -0,0 +1,6 @@
+#ifndef INC_FUNCTIONS_H
+#define INC_FUNCTIONS_H
+
+int add(int a, int b);
+
+#endif /* INC_FUNCTIONS_H */
