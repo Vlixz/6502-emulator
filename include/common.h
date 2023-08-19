8c414fff4b9202a449790a5d58b04bc17cb12176
vlixz <vlixz-programming@pm.me>
2023-08-19 14:56:54 +0200

    Function definitions

--- include/common.h
+++ include/common.h
@@ -52,4 +52,31 @@
 /* Extra cycles is added when a page is crossed */
 #define ADC_IN_Y_CYCLES 5
 
+// =======================================
+//             Logical AND
+// =======================================
+
+#define AND_IM_OPCODE 0x29
+#define AND_ZP_OPCODE 0x25
+#define AND_ZP_X_OPCODE 0x35
+#define AND_AB_OPCODE 0x2D
+#define AND_AB_X_OPCODE 0x3D
+#define AND_AB_Y_OPCODE 0x39
+#define AND_IN_X_OPCODE 0x21
+#define AND_IN_Y_OPCODE 0x31
+
+#define AND_IM_CYCLES 2
+#define AND_ZP_CYCLES 3
+#define AND_ZP_X_CYCLES 4
+#define AND_AB_CYCLES 4
+/* Extra cycles is added when a page is crossed */
+#define AND_AB_X_CYCLES 4
+/* Extra cycles is added when a page is crossed */
+#define AND_AB_Y_CYCLES 4
+#define AND_IN_X_CYCLES 6
+#define AND_AB_X_CYCLES 4
+#define AND_IN_Y_CYCLES 6
+/* Extra cycles is added when a page is crossed */
+#define AND_IN_Y_CYCLES 5
+
 #endif /* INC_COMMON_H*/
