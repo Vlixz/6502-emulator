bfa02a06d3d86bef7be9f8827b3ad5a914f65457
vlixz <vlixz-programming@pm.me>
2023-08-21 13:26:20 +0200

    BIT Unit Tests

--- test/BIT_tests.cpp
+++ test/BIT_tests.cpp
@@ -21,28 +21,211 @@ protected:
     }
 };
 
-TEST_F(BIT_TEST, BEQ_RE_NoZeroFlagSetNoBranch)
+#define BIT_ZP_TEST 1
+#define BIT_AB_TEST 1
+
+#if BIT_ZP_TEST
+
+TEST_F(BIT_TEST, BIT_ZP_CheckBitSixNoVNoN)
 {
-    cpu.Z = 0;
+    // A: 0010 0111
+    // M: 0010 0000
+    // R: 0010 0000
+
+    cpu.A = 0b00100111;
 
     // Start inline program
-    cpu.memory[0xFFFC] = BEQ_RE_OPCODE;
-    cpu.memory[0xFFFD] = 0x02; // Jump 2 bytes
+    cpu.memory[0xFFFC] = BIT_ZP_OPCODE;
+    cpu.memory[0xFFFD] = 0x0011;
+    cpu.memory[0x0011] = 0b00100000; // Bit mask
     // End inline program
 
-    int cycles = em6502_execute(&cpu, BEQ_RE_CYCLES);
+    int cycles = em6502_execute(&cpu, BIT_ZP_CYCLES);
 
-    ASSERT_EQ(cycles, BEQ_RE_CYCLES);
-
-    ASSERT_EQ(cpu.PC, 0xFFFE);
+    ASSERT_EQ(cycles, BIT_ZP_CYCLES);
 
     ASSERT_FALSE(cpu.Z);
 
+    ASSERT_EQ(cpu.V, 0);
+    ASSERT_EQ(cpu.N, 0);
+
     /* Make sure the rest are unaffected by the instruction */
     ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
-    ASSERT_EQ(cpu.N, NEGATIVE_FLAG_RESET_VALUE);
-    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
     ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
     ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
     ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
 }
+
+TEST_F(BIT_TEST, BIT_ZP_CheckBitSixVAndN)
+{
+    // A: 0010 0111
+    // M: 1110 0000
+    // R: 0010 0000
+
+    cpu.A = 0b00100111;
+
+    // Start inline program
+    cpu.memory[0xFFFC] = BIT_ZP_OPCODE;
+    cpu.memory[0xFFFD] = 0x0011;
+    cpu.memory[0x0011] = 0b11100000; // Bit mask
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, BIT_ZP_CYCLES);
+
+    ASSERT_EQ(cycles, BIT_ZP_CYCLES);
+
+    ASSERT_FALSE(cpu.Z);
+
+    ASSERT_EQ(cpu.V, 1);
+    ASSERT_EQ(cpu.N, 1);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+}
+
+TEST_F(BIT_TEST, BIT_ZP_CheckBitZero)
+{
+    // A: 0010 0111
+    // M: 0100 0000
+    // R: 0010 0000
+
+    cpu.A = 0b00100111;
+
+    // Start inline program
+    cpu.memory[0xFFFC] = BIT_ZP_OPCODE;
+    cpu.memory[0xFFFD] = 0x0011;
+    cpu.memory[0x0011] = 0b01000000; // Bit mask
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, BIT_ZP_CYCLES);
+
+    ASSERT_EQ(cycles, BIT_ZP_CYCLES);
+
+    ASSERT_TRUE(cpu.Z);
+
+    ASSERT_EQ(cpu.V, 1);
+    ASSERT_EQ(cpu.N, 0);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+}
+
+#endif /* BIT_ZP_TEST */
+
+#if BIT_AB_TEST
+
+TEST_F(BIT_TEST, BIT_AB_CheckBitSixNoVNoN)
+{
+    // A: 0010 0111
+    // M: 0010 0000
+    // R: 0010 0000
+
+    cpu.A = 0b00100111;
+
+    Word AbsoluteAddress = 0xFF11;
+
+    Byte LSB = AbsoluteAddress & BIT_MASK_FIRST_BYTE;
+    Byte MSB = AbsoluteAddress >> 8;
+
+    // Start inline program
+    cpu.memory[0xFFFC] = BIT_AB_OPCODE;
+    cpu.memory[0xFFFD] = LSB;
+    cpu.memory[0xFFFE] = MSB;
+    cpu.memory[AbsoluteAddress] = 0b00100000; // Bit mask
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, BIT_AB_CYCLES);
+
+    ASSERT_EQ(cycles, BIT_AB_CYCLES);
+
+    ASSERT_FALSE(cpu.Z);
+
+    ASSERT_EQ(cpu.V, 0);
+    ASSERT_EQ(cpu.N, 0);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+}
+
+TEST_F(BIT_TEST, BIT_AB_CheckBitSixVAndN)
+{
+    // A: 0010 0111
+    // M: 1110 0000
+    // R: 0010 0000
+
+    cpu.A = 0b00100111;
+
+    Word AbsoluteAddress = 0x7683;
+
+    Byte LSB = AbsoluteAddress & BIT_MASK_FIRST_BYTE;
+    Byte MSB = AbsoluteAddress >> 8;
+
+    // Start inline program
+    cpu.memory[0xFFFC] = BIT_AB_OPCODE;
+    cpu.memory[0xFFFD] = LSB;
+    cpu.memory[0xFFFE] = MSB;
+    cpu.memory[AbsoluteAddress] = 0b11100000; // Bit mask
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, BIT_AB_CYCLES);
+
+    ASSERT_EQ(cycles, BIT_AB_CYCLES);
+
+    ASSERT_FALSE(cpu.Z);
+
+    ASSERT_EQ(cpu.V, 1);
+    ASSERT_EQ(cpu.N, 1);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+}
+
+TEST_F(BIT_TEST, BIT_AB_CheckBitZero)
+{
+    // A: 0010 0111
+    // M: 0100 0000
+    // R: 0010 0000
+
+    cpu.A = 0b00100111;
+
+    Word AbsoluteAddress = 0xFDEF;
+
+    Byte LSB = AbsoluteAddress & BIT_MASK_FIRST_BYTE;
+    Byte MSB = AbsoluteAddress >> 8;
+
+    // Start inline program
+    cpu.memory[0xFFFC] = BIT_AB_OPCODE;
+    cpu.memory[0xFFFD] = LSB;
+    cpu.memory[0xFFFE] = MSB;
+    cpu.memory[AbsoluteAddress] = 0b01000000; // Bit mask
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, BIT_AB_CYCLES);
+
+    ASSERT_EQ(cycles, BIT_AB_CYCLES);
+
+    ASSERT_TRUE(cpu.Z);
+
+    ASSERT_EQ(cpu.V, 1);
+    ASSERT_EQ(cpu.N, 0);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+}
+
+#endif /* BIT_AB_TEST */
