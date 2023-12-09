6ddb7a18b7a6c13f8391935ab7573c07541d947b
vlixz <vlixz-programming@pm.me>
2023-12-09 10:41:14 +0100

    Added ZP_X tests

--- test/LDA_tests.cpp
+++ test/LDA_tests.cpp
@@ -21,6 +21,12 @@ protected:
     }
 };
 
+#define LDA_IM_TEST 1
+#define LDA_ZP_TEST 1
+#define LDA_ZP_X_TEST 1
+
+#if LDA_IM_TEST
+
 TEST_F(LDA_TEST, LDA_IM_LoadsCorrectValueIntoAccumulator)
 {
     // Start inline program
@@ -92,3 +98,169 @@ TEST_F(LDA_TEST, LDA_IM_SetsZeroFlag)
     ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
     ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
 }
+
+#endif // LDA_IM_TEST
+
+#if LDA_ZP_TEST
+
+TEST_F(LDA_TEST, LDA_ZP_LoadsCorrectValueIntoAccumulator)
+{
+    // Start inline program
+    cpu.memory[0xFFFC] = LDA_ZP_OPCODE;
+    cpu.memory[0xFFFD] = 0xF4;
+    cpu.memory[0x00F4] = 0x65;
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDA_ZP_CYCLES);
+
+    ASSERT_EQ(cycles, LDA_ZP_CYCLES);
+
+    ASSERT_EQ(cpu.A, 0x65);
+
+    ASSERT_EQ(cpu.Z, false);
+    ASSERT_EQ(cpu.N, false);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+TEST_F(LDA_TEST, LDA_ZP_SetsNegativeFlag)
+{
+    // Start inline program
+    cpu.memory[0xFFFC] = LDA_ZP_OPCODE;
+    cpu.memory[0xFFFD] = 0x64;
+    cpu.memory[0x0064] = -42; // 0xD6 (two compliment)
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDA_ZP_CYCLES);
+
+    ASSERT_EQ(cycles, LDA_ZP_CYCLES);
+
+    ASSERT_EQ(cpu.A, (unsigned char)-42); // 0xD6 (two compliment)
+
+    ASSERT_EQ(cpu.Z, false);
+    ASSERT_EQ(cpu.N, true);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+TEST_F(LDA_TEST, LDA_ZP_SetsZeroFlag)
+{
+    // Start inline program
+    cpu.memory[0xFFFC] = LDA_ZP_OPCODE;
+    cpu.memory[0xFFFD] = 0x41;
+    cpu.memory[0x0041] = 0x00;
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDA_ZP_CYCLES);
+
+    ASSERT_EQ(cycles, LDA_ZP_CYCLES);
+
+    ASSERT_EQ(cpu.A, 0x00);
+
+    ASSERT_EQ(cpu.Z, true);
+    ASSERT_EQ(cpu.N, false);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+#endif // LDA_ZP_TEST
+
+#if LDA_ZP_X_TEST
+
+TEST_F(LDA_TEST, LDA_ZP_X_LoadsCorrectValueIntoAccumulator)
+{
+    cpu.X = 0x10;
+
+    // Start inline program
+    cpu.memory[0xFFFC] = LDA_ZP_X_OPCODE;
+    cpu.memory[0xFFFD] = 0x56; // 0x56 + 0x10 = 0x66
+    cpu.memory[0x0066] = 0x65;
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDA_ZP_X_CYCLES);
+
+    ASSERT_EQ(cycles, LDA_ZP_X_CYCLES);
+
+    ASSERT_EQ(cpu.A, 0x65);
+
+    ASSERT_EQ(cpu.Z, false);
+    ASSERT_EQ(cpu.N, false);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+TEST_F(LDA_TEST, LDA_ZP_X_SetsNegativeFlag)
+{
+    cpu.X = 0x1;
+
+    // Start inline program
+    cpu.memory[0xFFFC] = LDA_ZP_X_OPCODE;
+    cpu.memory[0xFFFD] = 0x64; // 0x64 + 0x1 = 0x65
+    cpu.memory[0x0065] = -42;  // 0xD6 (two compliment)
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDA_ZP_X_CYCLES);
+
+    ASSERT_EQ(cycles, LDA_ZP_X_CYCLES);
+
+    ASSERT_EQ(cpu.A, (unsigned char)-42); // 0xD6 (two compliment)
+
+    ASSERT_EQ(cpu.Z, false);
+    ASSERT_EQ(cpu.N, true);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+TEST_F(LDA_TEST, LDA_ZP_X_SetsZeroFlag)
+{
+    cpu.X = 0xFF;
+
+    // Start inline program
+    cpu.memory[0xFFFC] = LDA_ZP_X_OPCODE;
+    cpu.memory[0xFFFD] = 0x80; // 0x80 + 0xFF = 0x17F (wrapps around so 0x7F)
+    cpu.memory[0x007F] = 0x00;
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDA_ZP_X_CYCLES);
+
+    ASSERT_EQ(cycles, LDA_ZP_X_CYCLES);
+
+    ASSERT_EQ(cpu.A, 0x00);
+
+    ASSERT_EQ(cpu.Z, true);
+    ASSERT_EQ(cpu.N, false);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+#endif // LDA_ZP_X_TEST
