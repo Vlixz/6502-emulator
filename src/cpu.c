9c9a7e611a69cfb4e6622a1a865ab1b9c8816f61
vlixz <vlixz-social@pm.me>
2024-01-23 13:17:43 +0100

    LDX Function implementation

--- src/cpu.c
+++ src/cpu.c
@@ -844,3 +844,58 @@ Byte DEY_IP(CPU_6502 *cpu) {
 
     return DEY_IP_CYCLES;
 }
+
+// =======================================
+//            Load X Register
+// =======================================
+
+Byte LDX_IM(CPU_6502 *cpu) {
+    Byte O = AddressingMode_Immediate(cpu->memory, &cpu->PC);
+
+    cpu->X = O;
+    cpu->Z = IS_ZERO(cpu->X);
+    cpu->N = IS_NEGATIVE(cpu->X);
+
+    return LDX_IM_CYCLES;
+}
+
+Byte LDX_ZP(CPU_6502 *cpu) {
+    Byte O = AddressingMode_ZeroPage(cpu->memory, &cpu->PC);
+
+    cpu->X = O;
+    cpu->Z = IS_ZERO(cpu->X);
+    cpu->N = IS_NEGATIVE(cpu->X);
+
+    return LDX_ZP_CYCLES;
+}
+
+Byte LDX_ZP_Y(CPU_6502 *cpu) {
+    Byte O = AddressingMode_ZeroPageY(cpu->memory, &cpu->PC, cpu->Y);
+
+    cpu->X = O;
+    cpu->Z = IS_ZERO(cpu->X);
+    cpu->N = IS_NEGATIVE(cpu->X);
+
+    return LDX_ZP_Y_CYCLES;
+}
+
+Byte LDX_AB(CPU_6502 *cpu) {
+    Byte O = AddressingMode_Absolute(cpu->memory, &cpu->PC);
+
+    cpu->X = O;
+    cpu->Z = IS_ZERO(cpu->X);
+    cpu->N = IS_NEGATIVE(cpu->X);
+
+    return LDX_AB_CYCLES;
+}
+
+Byte LDX_AB_Y(CPU_6502 *cpu) {
+    int cycles = 0;
+    Byte O = AddressingMode_AbsoluteY(cpu->memory, &cpu->PC, cpu->Y, &cycles);
+
+    cpu->X = O;
+    cpu->Z = IS_ZERO(cpu->X);
+    cpu->N = IS_NEGATIVE(cpu->X);
+
+    return LDX_AB_Y_CYCLES + cycles;
+}
