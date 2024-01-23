efe5a0ab5afcb014e19c91548700e94ef2bf1276
vlixz <vlixz-social@pm.me>
2024-01-23 16:51:39 +0100

    LDX_AB Unit Tests

--- test/LDX_tests.cpp
+++ test/LDX_tests.cpp
@@ -1,3 +1,4 @@
+#include "common.h"
 extern "C" {
 #include "6502.h"
 }
@@ -16,6 +17,7 @@ class LDX_TEST : public ::testing::Test {
 #define LDX_IM_TEST 1
 #define LDX_ZP_TEST 1
 #define LDX_ZP_Y_TEST 1
+#define LDX_AB_TEST 1
 
 #if LDX_IM_TEST
 
@@ -246,4 +248,83 @@ TEST_F(LDX_TEST, LDX_ZP_Y_SetsZeroFlag) {
     ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
 }
 
-#endif // LDA_ZP_Y_TEST
+#endif // LDX_ZP_Y_TEST
+
+#if LDX_AB_TEST
+
+TEST_F(LDX_TEST, LDX_AB_LoadsCorrectValueIntoXRegister) {
+    // Start inline program
+    cpu.memory[0xFFFC] = LDX_AB_OPCODE;
+    cpu.memory[0xFFFD] = 0x56;
+    cpu.memory[0xFFFE] = 0xFF; // 0xFF56
+    cpu.memory[0xFF56] = 0x65;
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDX_AB_CYCLES);
+
+    ASSERT_EQ(cycles, LDX_AB_CYCLES);
+
+    ASSERT_EQ(cpu.X, 0x65);
+
+    ASSERT_EQ(cpu.Z, false);
+    ASSERT_EQ(cpu.N, false);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+TEST_F(LDX_TEST, LDX_AB_SetsNegativeFlag) {
+    // Start inline program
+    cpu.memory[0xFFFC] = LDX_AB_OPCODE;
+    cpu.memory[0xFFFD] = 0x64;
+    cpu.memory[0xFFFE] = 0x01; // 0x0164
+    cpu.memory[0x0164] = -42;  // 0xD6 (two compliment)
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDX_AB_CYCLES);
+
+    ASSERT_EQ(cycles, LDX_AB_CYCLES);
+
+    ASSERT_EQ(cpu.X, (unsigned char)-42); // 0xD6 (two compliment)
+
+    ASSERT_EQ(cpu.Z, false);
+    ASSERT_EQ(cpu.N, true);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+TEST_F(LDX_TEST, LDX_AB_SetsZeroFlag) {
+    // Start inline program
+    cpu.memory[0xFFFC] = LDX_AB_OPCODE;
+    cpu.memory[0xFFFD] = 0x80;
+    cpu.memory[0xFFFE] = 0x00; // 0x0080
+    cpu.memory[0x0080] = 0x00;
+    // End inline program
+
+    int cycles = em6502_execute(&cpu, LDX_AB_CYCLES);
+
+    ASSERT_EQ(cycles, LDX_AB_CYCLES);
+
+    ASSERT_EQ(cpu.A, 0x00);
+
+    ASSERT_EQ(cpu.Z, true);
+    ASSERT_EQ(cpu.N, false);
+
+    /* Make sure the rest are unaffected by the instruction */
+    ASSERT_EQ(cpu.I, INTERRUPT_DISABLE_RESET_VALUE);
+    ASSERT_EQ(cpu.C, CARRY_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.V, OVERFLOW_FLAG_RESET_VALUE);
+    ASSERT_EQ(cpu.B, BREAK_COMMAND_RESET_VALUE);
+    ASSERT_EQ(cpu.D, DECIMAL_MODE_RESET_VALUE);
+}
+
+#endif // LDX_AB_TEST
